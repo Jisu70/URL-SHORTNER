c9cd46c30c85920f85e717699e4053c15c98ba01
Jisu70 <126372181+Jisu70@users.noreply.github.com>
2023-09-20 15:07:46 +0000

    Update urlController.js and url.js

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -31,4 +31,22 @@ const generateShortUrl = async (req, res) => {
     }
 };
 
+const getUrl = async (req, res) => {
+  try {
+    const url = await Url.findOne({ urlId: req.params.urlId });
+    if (url) {
+      await Url.updateOne(
+        {
+          urlId: req.params.urlId,
+        },
+        { $inc: { clicks: 1 } }
+      );
+      return res.redirect(url.origUrl);
+    } else res.status(404).json('Not found');
+  } catch (err) {
+    console.log(err);
+    res.status(500).json('Server Error');
+  }
+};
+
 export { generateShortUrl };
--- routes/url.js
+++ routes/url.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 
 router.post('/', generateShortUrl)
+router.get('/:urlId', generateShortUrl)
 
 export default router; 
 
